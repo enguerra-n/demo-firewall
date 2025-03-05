42ae5bb69a0a7999357c30356067832f51887d99
enguerra-n <larminateur@gmail.com>
2025-03-05 16:08:17 +0100

    ajustement

--- scripts/construct-vm.sh
+++ scripts/construct-vm.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+$folderserver = ../sae_Firewall/server
+$folderclient = ../sae_Firewall/client
+
+
+# check Vagrant install
+if ! command -v vagrant &> /dev/null; then
+    echo " Vagrant n'est pas installé."
+    exit 1
+else
+    echo " Vagrant est installé : $(vagrant -v)"
+fi
+
+# check VirtualBox install
+if ! command -v VBoxManage &> /dev/null; then
+    echo " VirtualBox n'est pas installé."
+    exit 1
+else
+    echo " VirtualBox est installé : $(VBoxManage -v)"
+fi
+
+
+### pushd et popd permet de changer de dossier "temporairement"
+pushd $folderserver
+vagrant up
+popd
+
+pushd $folderclient
+vagrant up
+popd
