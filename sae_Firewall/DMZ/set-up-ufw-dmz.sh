f85056bf37f19c61c30bbaf74be2746de9b0850e
enguerra-n <larminateur@gmail.com>
2025-03-11 11:25:37 +0100

    update

--- sae_Firewall/DMZ/set-up-ufw-dmz.sh
+++ sae_Firewall/DMZ/set-up-ufw-dmz.sh
@@ -17,9 +17,16 @@ sudo ufw allow out on eth0
 # Accepter le trafic depuis le réseau 192.168.56.0/24 sur eth1
 sudo ufw allow from 192.168.56.0/24 to any
 
-# Autoriser HTTP (port 80) et HTTPS (port 443) sur eth2 pour l'adresse 192.168.56.100
+# autoriser 80 et 443 sur eth2 pour le serveur
 sudo ufw allow in on eth2 to 192.168.56.100 port 80
 sudo ufw allow in on eth2 to 192.168.56.100 port 443
+
+#autoriser le LAN a ssh le srv seulement
+sudo ufw allow in on eth1 to 192.168.56.100 port 22
+
+
+sudo ufw allow in on eth1 to any port 80
+sudo ufw allow in on eth1 to any port 443
 
 # Autoriser le routage entre eth1 et eth2
 sudo ufw route allow in on eth1 to out on eth2
@@ -31,4 +38,4 @@ sudo sed -i '/^# allow all icmp/a -A ufw-before-output -p icmp --icmp-type echo-
 
 # Activer UFW (répondre "yes" automatiquement)
 yes y | sudo ufw enable
-#sudo ufw enable
+sudo ufw reload
